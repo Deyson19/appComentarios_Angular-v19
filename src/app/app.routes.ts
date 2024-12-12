3ea41dd0039233878dc9141bc091d4a8c1929d67
Deyson Venté <deiandresvent@hotmail.com>
2024-12-12 10:34:44 -0500

    Define routes for publicaciones, comentarios, and favoritos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,57 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'publicaciones',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/publicaciones/publicaciones.component').then(
+            (m) => m.PublicacionesComponent
+          ),
+      },
+      {
+        path: ':id',
+        title: 'Publicación',
+        async loadComponent() {
+          return await import('./pages/publicacion/publicacion.component').then(
+            (m) => m.PublicacionComponent
+          );
+        },
+      },
+    ],
+  },
+  {
+    path: 'comentarios',
+    children: [
+      {
+        path: '',
+        async loadComponent() {
+          const c = await import(
+            './pages/comentarios/comentarios.component'
+          ).then((m) => m.ComentariosComponent);
+          return c;
+        },
+      },
+      {
+        path: ':id',
+        async loadComponent() {
+          return await import('./pages/comentario/comentario.component').then(
+            (_) => _.ComentarioComponent
+          );
+        },
+      },
+    ],
+  },
+  {
+    path: 'favoritos',
+    loadComponent: () =>
+      import('./pages/favoritos/favoritos.component').then((m) => m.default),
+  },
+  {
+    path: '**',
+    redirectTo: '/publicaciones',
+    pathMatch: 'full',
+  },
+];
