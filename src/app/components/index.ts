a46f726d81efec7eef55173a3741709b703c0ba8
Deyson Venté <deiandresvent@hotmail.com>
2024-12-13 11:51:18 -0500

    Add index file to export components

---
+++ src/app/components/index.ts
@@ -0,0 +1,6 @@
+export * from './comentario-container/comentario-container.component';
+export * from './comentarioCard/comentarioCard.component';
+export * from './favoritoItem/favoritoItem.component';
+export * from './modal-nuevo-comentario/modal-nuevo-comentario.component';
+export * from './publicacion-card/publicacion-card.component';
+export * from './spinner/spinner.component';
