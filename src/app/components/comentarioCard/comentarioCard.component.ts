9d2821f58aab72da6e04c207f243fba653f41a49
Deyson Venté <deiandresvent@hotmail.com>
2024-12-12 20:34:02 -0500

    card para iterar la lista de comentarios

---
+++ src/app/components/comentarioCard/comentarioCard.component.ts
@@ -0,0 +1,39 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+  input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { ComentarioService } from '../../core/services';
+import { IComentario } from '../../core/interfaces';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-comentario-card',
+  imports: [RouterLink, CommonModule],
+  templateUrl: './comentarioCard.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ComentarioCardComponent implements OnChanges {
+  private readonly _comentariosService = inject(ComentarioService);
+  comentario = input.required<IComentario>();
+  constructor(private changeDf: ChangeDetectorRef) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    let _comentario = changes['comentario'].currentValue;
+    if (_comentario) {
+      this._comentariosService.getComentario(this.comentario().id).subscribe({
+        next: (comentario) => {
+          this.comentario()!.publicacion = comentario.publicacion;
+          this.changeDf.markForCheck();
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
+    }
+  }
+}
