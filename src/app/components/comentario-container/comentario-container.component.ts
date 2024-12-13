eb416e58123d5727b179bc912feaae55c720eef8
Deyson Venté <deiandresvent@hotmail.com>
2024-12-13 11:43:56 -0500

    Fix typo in deleteComentario method and enhance functionality

--- src/app/components/comentario-container/comentario-container.component.ts
+++ src/app/components/comentario-container/comentario-container.component.ts
@@ -1,6 +1,13 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { IComentario } from '../../core/interfaces';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
+import { ComentarioService } from '@Services/index';
+import { IComentario, IPublicacion } from '@Interfaces/index';
 
 @Component({
   selector: 'app-comentario-container',
@@ -9,11 +16,28 @@ import { CommonModule } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ComentarioContainerComponent {
+  private readonly toast = inject(ToastrService);
+
+  private readonly _comentarioService = inject(ComentarioService);
+  public publicacion = input.required<IPublicacion>();
+  private favoritos: IComentario[] = [];
+
   comentario = input.required<IComentario>();
   addToFavorite(id: string) {
-    console.log('Agregando a favoritos');
+    const comentario = this.publicacion()?.comentarios?.find(
+      (x) => x.id === id
+    );
+    if (comentario) {
+      this.toast.success('Comentario agregado a favoritos');
+      this._comentarioService.addToFavorite(comentario);
+    } else {
+      this.toast.error('Error al agregar comentario a favoritos');
+    }
   }
-  deleteComment(id: string) {
-    console.log('Eliminando comentario');
+  deleteComentario(id: string) {
+    if (id) {
+      this._comentarioService.removeFavorite(id);
+      this.toast.warning('Comentario eliminado de favoritos');
+    }
   }
 }
