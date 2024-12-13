27ecc642e65e77cd06bf10a05f21dfc630354529
Deyson Venté <deiandresvent@hotmail.com>
2024-12-13 11:36:57 -0500

    Update publicacion-card component HTML and TypeScript
    - Commented out the comments section temporarily.
    - Updated import paths for `IPublicacion` and `ImageHelper` to use aliases.

--- src/app/components/publicacion-card/publicacion-card.component.ts
+++ src/app/components/publicacion-card/publicacion-card.component.ts
@@ -7,9 +7,8 @@ import {
   SimpleChanges,
 } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IPublicacion } from '../../core/interfaces';
-import { ImageHelper } from '../../core/helpers/ImageHelper';
-
+import { ImageHelper } from '@Helpers/ImageHelper';
+import { IPublicacion } from '@Interfaces/index';
 @Component({
   selector: 'app-publicacion-card',
   imports: [RouterLink, CommonModule],
@@ -20,7 +19,7 @@ export class PublicacionCardComponent implements OnChanges {
   public publicacion = input.required<IPublicacion>();
   public isDetails = input<boolean>(false);
   ngOnChanges(changes: SimpleChanges): void {
-    let post = changes as unknown as IPublicacion;
+    const post = changes as unknown as IPublicacion;
     if (post) {
       const getImage = ImageHelper.getImages();
       this.publicacion().imagen = getImage;
