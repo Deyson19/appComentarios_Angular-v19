fb5d5fea69af1de5045b71747374c46b05f510a5
Deyson Venté <deiandresvent@hotmail.com>
2024-12-12 21:01:33 -0500

    Obtener un avatar aleatorio y reutilizar un metodo para obtener un item del listado.

--- src/app/core/helpers/ImageHelper.ts
+++ src/app/core/helpers/ImageHelper.ts
@@ -6,10 +6,28 @@ export class ImageHelper {
     'https://cdn.pixabay.com/photo/2017/08/05/22/43/newspaper-2586311_1280.jpg',
     'https://cdn.pixabay.com/photo/2016/08/13/23/13/news-1591767_1280.jpg',
   ];
+  private static readonly avatars: string[] = [
+    'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(11).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(12).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(29).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp',
+  ];
+
+  private static getIndex(elements: string[]): number {
+    let random = Math.random();
+    let item = Math.floor(random * elements.length);
+    return item;
+  }
 
   public static getImages(): string {
-    let random = Math.random();
-    let item = Math.floor(random * this.images.length);
-    return this.images[item];
+    let image = this.getIndex(this.images);
+    return this.images[image];
+  }
+  public static getAvatar(): string {
+    return this.avatars[this.getIndex(this.avatars)];
   }
 }
